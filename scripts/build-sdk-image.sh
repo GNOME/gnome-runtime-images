7b5b1f3f4da48c590b254e52599cdc9364aa1d4d
Jordan Petridis <jpetridis@gnome.org>
2025-06-17 20:47:30 +0300

    Add typescript Sdk Extension to the images
    Typescript apps are getting popular

--- scripts/build-sdk-image.sh
+++ scripts/build-sdk-image.sh
@@ -38,6 +38,11 @@ fi
 buildah run "$CONTAINER" flatpak install --user --noninteractive \
     "org.freedesktop.Sdk.Extension.rust-stable//${FD_BRANCH}"
 
+    buildah run "$CONTAINER" flatpak install --user --noninteractive \
+    "org.freedesktop.Sdk.Extension.node20//${FD_BRANCH}" \
+    "org.freedesktop.Sdk.Extension.typescript//${FD_BRANCH}"
+
+
 buildah run "$CONTAINER" flatpak install --user --noninteractive \
     "org.freedesktop.Sdk//${FD_BRANCH}"
 
