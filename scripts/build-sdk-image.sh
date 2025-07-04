5473d5bd2b83c64d1aa928a0f1c3bf3a05dd0f2a
Jordan Petridis <jpetridis@gnome.org>
2025-07-04 16:25:37 +0300

    Instann vala-nightly again
    Disabled cause we needed an image with the Sdk published before we can build and publish the extension.
    
    Let's reenable it now

--- scripts/build-sdk-image.sh
+++ scripts/build-sdk-image.sh
@@ -43,8 +43,8 @@ buildah run "$CONTAINER" flatpak install --user --noninteractive \
 buildah run "$CONTAINER" flatpak install --user --noninteractive \
     "org.freedesktop.Sdk//${FD_BRANCH}"
 
-# buildah run $CONTAINER flatpak install --user --noninteractive \
-#     "org.freedesktop.Sdk.Extension.vala-nightly//${FD_BRANCH}"
+buildah run $CONTAINER flatpak install --user --noninteractive \
+    "org.freedesktop.Sdk.Extension.vala-nightly//${FD_BRANCH}"
 
 buildah run "$CONTAINER" flatpak info --user "org.gnome.Platform//${BRANCH}"
 buildah run "$CONTAINER" flatpak info --user "org.gnome.Sdk//${BRANCH}"
